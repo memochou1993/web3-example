88a1b162323d23936402452f4fcad1d0a3ecbb81
Memo Chou <memochou1993@gmail.com>
2022-02-23 23:44:53 +0800

    Update script

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
 async function main() {
+  const { API_URL } = process.env;
   const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
-  const web3 = createAlchemyWeb3(`https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`);
+  const web3 = createAlchemyWeb3(API_URL);
   const blockNumber = await web3.eth.getBlockNumber();
   console.log('The latest block number is ' + blockNumber);
-}
+};
+
 main();
